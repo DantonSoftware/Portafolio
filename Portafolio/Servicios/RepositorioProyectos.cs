0b170fecc2e59828709a56615b84732bc72f220e
danton <danton.cortes@gmail.com>
2023-07-17 14:44:39 -0400

    Se agrega el proyecto

---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,46 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+    public class RepositorioProyectos: IRepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP.NET Core",
+                    Link = "http://amazon.com",
+                    ImagenUrl = "/imagenes/amazon.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Pagina de noticias en React",
+                    Link = "http://nytimes.com",
+                    ImagenUrl = "/imagenes/nyt.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red Social para compartir en comunidades",
+                    Link = "http://reddit.com",
+                    ImagenUrl = "/imagenes/reddit.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en linea para comprar videojuegos",
+                    Link = "http://store.steampowered.com",
+                    ImagenUrl = "/imagenes/steam.png"
+                },
+            };
+        }
+    }
+}
---
+++ Portafolio/Servicios/ServicioEmail.cs
@@ -0,0 +1,42 @@
+﻿using Portafolio.Models;
+using System.Net.Mail;
+
+namespace Portafolio.Servicios
+{
+    public interface IServicioEmail
+    {
+        Task Enviar(ContactoViewModel contacto);
+    }
+    public class ServicioEmail: IServicioEmail
+    {
+        private readonly IConfiguration configuration;
+
+        public ServicioEmail(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task Enviar(ContactoViewModel contacto)
+        {
+            var email = configuration.GetValue<string>("SENDGRID_FROM");
+            var nombre = configuration.GetValue<string>("SENDGRID_NOMBRE");
+            var Password = configuration.GetValue<string>("SENDGRID_PASSWORD");
+            //quitar los espacios entre smtp y antes del com
+            SmtpClient client = new SmtpClient("smtp.office365.com", 587);
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+
+            client.Credentials = new System.Net.NetworkCredential(email, Password);
+
+            var from = email;
+            var subject = $"El cliente {contacto.Email} quiere contactarte ";
+            var to = email;
+            var mensajeTextoPlano = contacto.Mensaje;
+            var contenidoHtml = @$"De: {contacto.Nombre} - Email: {contacto.Email} Mensaje: {contacto.Mensaje}";
+            MailMessage message = new MailMessage(from, to, subject, contenidoHtml);
+            client.Send(message);
+        }
+
+
+    }
+}
